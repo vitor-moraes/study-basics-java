e33033bc9c0d43eadbac082a17d64adeaa7693eb
Vitor Moraes <vitor.oliveira.moraes@gmail.com>
2025-11-11 07:51:36 -0300

    refactor: supress unused warning

--- StringConcatenationPerformance.java
+++ StringConcatenationPerformance.java
@@ -1,14 +1,15 @@
-public class StringConcatenationPerformance{
-    public static void main(String[] args){ 
+public class StringConcatenationPerformance {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
         String slowConcat = "Hello";
         long startTimeToSlowConcat = System.currentTimeMillis();
-        for(int i = 0; i < 10000; i++){
+        for (int i = 0; i < 10000; i++) {
             slowConcat += "Hello";
         }
         long durationToSlowConcat = System.currentTimeMillis() - startTimeToSlowConcat;
-        long startTimeToFastConcat = System.currentTimeMillis();  
+        long startTimeToFastConcat = System.currentTimeMillis();
         StringBuilder sb = new StringBuilder("Hello");
-        for(int i = 0; i < 10000; i++){
+        for (int i = 0; i < 10000; i++) {
             sb.append("Hello");
         }
         long durationToFastConcat = System.currentTimeMillis() - startTimeToFastConcat;
