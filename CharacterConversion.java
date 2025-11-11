30d3aacdfc05cae3836ed3e9e76476020e8f32c0
Vitor Moraes <vitor.oliveira.moraes@gmail.com>
2025-11-10 21:19:41 -0300

    feat: character conversion

---
+++ CharacterConversion.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CharacterConversion {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String word = scanner.nextLine();
+        char[] charWord = word.toCharArray();
+        System.out.println("Characters: " + Arrays.toString(charWord));
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        String numberString = Integer.toString(number);
+        System.out.println("As string: " + numberString);
+        System.out.println("As integer + 1: " + (number + 1));
+        scanner.close();
+    }
+}
